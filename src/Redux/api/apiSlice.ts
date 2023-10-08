a5e72fe1cf89cc626f4121bc61498baf9c04d8d8
Anish Kumar <anishbishnoi127@gmail.com>
2023-10-08 14:02:02 +0530

    Created Rtk Query Api Endpoints

---
+++ src/Redux/api/apiSlice.ts
@@ -0,0 +1,51 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://65225aeaf43b1793841469d2.mockapi.io",
+  }),
+  tagTypes: ["Users"],
+  endpoints: (builder) => ({
+    fetchUser: builder.query({
+      query: () => ({
+        url: "/users",
+        method: "GET",
+      }),
+    }),
+    fetchSingleUser: builder.query({
+      query: (id) => ({
+        url: `/users/:${id}`,
+        method: "GET",
+      }),
+    }),
+    setUser: builder.mutation({
+      query: (data) => ({
+        url: "/users",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    updateUser: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/users/:${id}`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `/users/:${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useSetUserMutation,
+  useFetchSingleUserQuery,
+  useDeleteUserMutation,
+  useFetchUserQuery,
+  useUpdateUserMutation,
+} = apiSlice;
